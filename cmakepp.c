bac92d3e2c074a3c8af378c0819bd7f9bca8344c
Daniel Burke <danielseanburke@gmail.com>
2024-10-06 15:54:05 -0700

    Move doc comment to ifdef area and move rom to roms folder

--- cmakepp.c
+++ cmakepp.c
@@ -1,3 +1,12 @@
+#ifndef __CMAKEPP_C_CODE
+gcc -D__CMAKEPP_C_CODE -D__CMAKEPP_COMPILE_MYSELF cmakepp.c -o cmakepp && ./cmakepp && rm ./cmakepp
+exit $?
+#endif
+
+// So that if we use globbing to compile source files,
+// we don't include cmakepp.c
+#ifdef __CMAKEPP_COMPILE_MYSELF
+
 /* -------------------------------------------------------------------------------
 
     /      \ /  \     /  | /      \ /  |  /  |/        |/       \ /       \
@@ -15,7 +24,7 @@
    a header file.
 
    To run the build, just "run" this file (./cmakepp.c), there is a small script
-   embedded below that will compile this file, run it to compile the project
+   embedded above that will compile this file, run it to compile the project
    files, and then remove itself.
 
    Build variables:
@@ -33,14 +42,6 @@
    -----------------------------------------------------------------------------*/
 
 
-#ifndef __CMAKEPP_C_CODE
-gcc -D__CMAKEPP_C_CODE -D__CMAKEPP_COMPILE_MYSELF cmakepp.c -o cmakepp && ./cmakepp && rm ./cmakepp
-exit $?
-#endif
-
-// So that if we use globbing to compile source files,
-// we don't include cmakepp.c
-#ifdef __CMAKEPP_COMPILE_MYSELF
 
 #include <errno.h>
 #include <stdio.h>
@@ -104,7 +105,6 @@ int main(int argc, char **argv)
         i++;
         lib_name = libs[i];
     }
-    printf("\n");
 
     sprintf(compile_command + offset, "-o %s", target);
     offset += strlen("-o ") + strlen(target);
